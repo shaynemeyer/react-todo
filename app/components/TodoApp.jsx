741ebcbc70f9c2e6bad6832b368ae2a326e276fa
Shayne Meyer <shayne.meyer@gmail.com>
2017-06-28 07:49:20 -0700

    Refactor app to work with localStorage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,62 +1,20 @@
 var React = require('react');
-var uuid = require('node-uuid');
-var moment = require('moment');
 
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
-var TodoAPI = require('TodoAPI');
 
 class TodoApp extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      showCompleted: false,
-      searchText: '',
-      todos: TodoAPI.getTodos()
-    }
-  }
-
-  componentDidUpdate = () => {
-    TodoAPI.setTodos(this.state.todos);
-  }
-
-  handleAddTodo = (text) => {
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {
-          id: uuid(),
-          text,
-          completed: false,
-          createdAt: moment().unix(),
-          completedAt: undefined
-        }
-      ]
-    });
-  }
-
-  handleSearch = (showCompleted, searchText) => {
-    this.setState({
-      showCompleted: showCompleted,
-      searchText: searchText.toLowerCase()
-    });
-  }
-
   render(){
-    var {todos, showCompleted, searchText} = this.state;
-    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
-
     return (
       <div>
         <h1 className="page-title">Todo App</h1>
-
         <div className="row">
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
-              <TodoSearch onSearch={this.handleSearch}/>
+              <TodoSearch/>
               <TodoList />
-              <AddTodo onAddTodo={this.handleAddTodo}/>
+              <AddTodo/>
             </div>
           </div>
         </div>
