19718d36e16f1dd282483b4ce191429d21905100
Shayne Meyer <shayne.meyer@gmail.com>
2017-06-29 07:32:51 -0700

    store new todos on firebase using thunk

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -1,5 +1,10 @@
-var expect = require('expect');
-var actions = require('actions');
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import expect from 'expect';
+import * as actions from 'actions';
+
+var createMockStore = configureMockStore([thunk]);
 
 describe('Actions', () => {
   it('should generate search text action', () => {
@@ -23,11 +28,32 @@ describe('Actions', () => {
   it('should generate add todo action', () => {
     var action = {
       type: 'ADD_TODO',
-      text: 'Thing to do'
+      todo: {
+        id: '123abc',
+        text: 'Thing to do',
+        completed: false,
+        createdAt: 0
+      }
     };
 
-    var res = actions.addTodo(action.text);
+    var res = actions.addTodo(action.todo);
     expect(res).toEqual(action);
+  });
+
+  it('should create todo and dispatch ADD_TODO', (done) => {
+    const store = createMockStore({});
+    const todoText = 'My todo item';
+
+    store.dispatch(actions.startAddTodo(todoText)).then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toInclude({
+        type: 'ADD_TODO'
+      });
+      expect(actions[0].todo).toInclude({
+        text: todoText
+      });
+      done();
+    }).catch(done);
   });
 
   it('should generate add todos action object', () => {
