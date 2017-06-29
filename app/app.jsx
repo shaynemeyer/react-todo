03c1d83b23f9de816c39681dc7097d1635122e7e
Shayne Meyer <shayne.meyer@gmail.com>
2017-06-29 13:01:36 -0700

    Fetch todos from firebase on app start

--- app/app.jsx
+++ app/app.jsx
@@ -7,16 +7,8 @@ var actions = require('actions');
 var store = require('configureStore').configure();
 var TodoAPI = require('TodoAPI');
 
-// import './../playground/firebase/index';
 
-store.subscribe(() => {
-  var state = store.getState();
-  console.log('New state', state);
-  TodoAPI.setTodos(state.todos);
-});
-
-var initialTodos = TodoAPI.getTodos();
-store.dispatch(actions.addTodos(initialTodos));
+store.dispatch(actions.startAddTodos());
 
 // Load foundation
 $(document).foundation();
--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -91,13 +91,18 @@ describe('Actions', () => {
     var testTodoRef;
 
     beforeEach((done) => {
-      testTodoRef = firebaseRef.child('todos').push();
+      var todosRef = firebaseRef.child('todos');
+      todosRef.remove().then(() => {
+        testTodoRef = firebaseRef.child('todos').push();
 
-      testTodoRef.set({
-        text: 'Something to do',
-        completed: false,
-        createdAt: 232343
-      }).then(() => done());
+        testTodoRef.set({
+          text: 'Something to do',
+          completed: false,
+          createdAt: 232343
+        })
+      })
+        .then(() => done())
+        .catch(done);
     });
 
     afterEach((done) => {
@@ -118,6 +123,21 @@ describe('Actions', () => {
         done();
       }, done);
 
+    });
+
+    it('should populate todos and dispatch ADD_TODOS', (done) => {
+      const store = createMockStore({});
+      const action = actions.startAddTodos();
+
+      store.dispatch(action).then(() => {
+        const mockActions = store.getActions();
+
+        expect(mockActions[0].type).toEqual('ADD_TODOS');
+        expect(mockActions[0].todos.length).toEqual(1);
+        expect(mockActions[0].todos[0].text).toEqual('Something to do');
+
+        done();
+      })
     });
   });
 });
